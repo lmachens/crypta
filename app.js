bb220822c3a086016ff7a67142fae0ddb0096a3a
Leon Machens <leon.machens@googlemail.com>
2020-03-23 13:57:17 +0100

    Read command from process args (#2)
    * Read command from process args
    
    * Remove db.json

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
-function sayHi() {
-  console.log("Irgendwas anderes");
+const [command, key, value] = process.argv.slice(2);
+
+function get() {
+  console.log("Called GET", key);
+  // Read and log db.json
+  console.log("db");
 }
 
-sayHi();
+function set() {
+  console.log("Called SET", key, value);
+}
+
+if (command === "get") {
+  get();
+} else if (command === "set") {
+  set();
+} else {
+  console.error("Unknown command");
+}
