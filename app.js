9dfc60e78d01107be74a36e89516df8e185c3cb8
Leon Machens <leon.machens@googlemail.com>
2020-03-23 16:23:04 +0100

    Read and write db.json (#3)
    * Get password by key
    
    * Set password
    
    * Unset password
    
    * Remove log value from unset

--- app.js
+++ app.js
@@ -1,19 +1,54 @@
+const fs = require("fs");
+
 const [command, key, value] = process.argv.slice(2);
 
 function get() {
   console.log("Called GET", key);
   // Read and log db.json
-  console.log("db");
+  try {
+    // Read db.json
+    const passswordsJSON = fs.readFileSync("./db.json", "utf8");
+    const passwords = JSON.parse(passswordsJSON);
+    // Log password
+    console.log(key, passwords[key]);
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 function set() {
   console.log("Called SET", key, value);
+  try {
+    // Read db.json
+    const passwordsJSON = fs.readFileSync("./db.json", "utf8");
+    const passwords = JSON.parse(passwordsJSON);
+    // Update value by key
+    passwords[key] = value;
+    // Write db.json
+    fs.writeFileSync("./db.json", JSON.stringify(passwords, null, 2));
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+function unset() {
+  console.log("Called UNSET", key);
+  try {
+    const passwordsJSON = fs.readFileSync("./db.json", "utf8");
+    const passwords = JSON.parse(passwordsJSON);
+    delete passwords[key];
+    fs.writeFileSync("./db.json", JSON.stringify(passwords, null, 2));
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 if (command === "get") {
   get();
 } else if (command === "set") {
   set();
+} else if (command === "unset") {
+  unset();
 } else {
   console.error("Unknown command");
 }
