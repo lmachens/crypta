d4a0501032a54909f8862cc496613d4e563ef289
lmachens <leon.machens@googlemail.com>
2020-03-24 11:02:29 +0100

    Ask for password

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const { get, set, unset } = require("./lib/commands");
+const { askForPassword } = require("./lib/questions");
 
-const [command, key, value] = process.argv.slice(2);
+const [command, key] = process.argv.slice(2);
 
-if (command === "get") {
-  get(key);
-} else if (command === "set") {
-  set(key, value);
-} else if (command === "unset") {
-  unset(key);
-} else {
-  console.error("Unknown command");
+async function run() {
+  if (command === "get") {
+    get(key);
+  } else if (command === "set") {
+    const password = await askForPassword(key);
+    set(key, password);
+  } else if (command === "unset") {
+    unset(key);
+  } else {
+    console.error("Unknown command");
+  }
 }
+
+run();
