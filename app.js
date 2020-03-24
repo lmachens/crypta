9bae1163848a4b94e70c130865ad033ff193a90f
lmachens <leon.machens@googlemail.com>
2020-03-24 16:10:30 +0100

    Hash master password and add reset function

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
-const { get, set, unset } = require("./lib/commands");
+const { get, set, unset, reset } = require("./lib/commands");
 const { askForPassword, askForMasterPassword } = require("./lib/questions");
 const { readMasterPassword } = require("./lib/passwords");
+const { verifyHash } = require("./lib/crypto");
 
 const [command, key] = process.argv.slice(2);
 
 async function run() {
   const answeredMasterPassword = await askForMasterPassword();
+
+  if (command === "reset") {
+    return reset(answeredMasterPassword);
+  }
   const masterPassword = readMasterPassword();
 
-  if (answeredMasterPassword !== masterPassword) {
+  if (!verifyHash(answeredMasterPassword, masterPassword)) {
     console.error("Fuck off!");
     return;
   }
