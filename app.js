997f6d2561a339615206260a83f0e7e02864c3d4
lmachens <leon.machens@googlemail.com>
2020-03-25 16:00:26 +0100

    Connect to mongodb

--- app.js
+++ app.js
@@ -10,48 +10,57 @@ const {
   askForMasterPassword,
   askQuestion,
 } = require("./lib/questions");
-const { readMasterPassword } = require("./lib/passwords");
+const { getMasterPassword } = require("./lib/queries");
 const { verifyHash } = require("./lib/crypto");
+const { connect, close } = require("./lib/db");
 
 const [command, key] = process.argv.slice(2);
 
 async function run() {
-  const answeredMasterPassword = await askForMasterPassword();
+  try {
+    // Connect with db
+    await connect();
+    const answeredMasterPassword = await askForMasterPassword();
 
-  if (command === "reset") {
-    return reset(answeredMasterPassword);
-  }
+    if (command === "reset") {
+      return reset(answeredMasterPassword);
+    }
 
-  if (command === "change") {
-    const answer = await askQuestion(
-      "Are you sure you want to change your master password? (y/n)"
-    );
-    if (answer !== "y") {
+    if (command === "change") {
+      const answer = await askQuestion(
+        "Are you sure you want to change your master password? (y/n)"
+      );
+      if (answer !== "y") {
+        return;
+      }
+
+      const newMasterPassword = await askQuestion(
+        "Please enter new master password:"
+      );
+      return changeMasterPassword(newMasterPassword);
+    }
+
+    const masterPassword = await getMasterPassword();
+
+    if (!verifyHash(answeredMasterPassword, masterPassword)) {
+      console.error("Fuck off!");
       return;
     }
 
-    const newMasterPassword = await askQuestion(
-      "Please enter new master password:"
-    );
-    return changeMasterPassword(newMasterPassword);
-  }
-
-  const masterPassword = readMasterPassword();
-
-  if (!verifyHash(answeredMasterPassword, masterPassword)) {
-    console.error("Fuck off!");
-    return;
-  }
-
-  if (command === "get") {
-    get(key);
-  } else if (command === "set") {
-    const password = await askForPassword(key);
-    set(key, password);
-  } else if (command === "unset") {
-    unset(key);
-  } else {
-    console.error("Unknown command");
+    if (command === "get") {
+      get(key, masterPassword);
+    } else if (command === "set") {
+      const password = await askForPassword(key);
+      set(key, password, masterPassword);
+    } else if (command === "unset") {
+      unset(key);
+    } else {
+      console.error("Unknown command");
+    }
+  } catch (error) {
+    console.error(error);
+  } finally {
+    await close();
   }
 }
 
