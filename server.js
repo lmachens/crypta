1ce4656ba8316689fe9f9fb75f9e3682fa1486bd
lmachens <leon.machens@googlemail.com>
2020-03-26 15:12:42 +0100

    Connect to database

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { connect } = require("./lib/db");
 
 const app = express();
 const port = 8000;
@@ -12,6 +13,20 @@ app.get("/passwords/:name", (request, response) => {
   response.send(`${name} password:`);
 });
 
-app.listen(port, () => {
-  console.log(`Server is running http://localhost:${port}`);
+// async function startServer() {
+//   await connect();
+//   console.log("Database is connected");
+
+//   app.listen(port, () => {
+//     console.log(`Server is running http://localhost:${port}`);
+//   });
+// }
+// startServer();
+
+connect().then(() => {
+  console.log("Database is connected");
+
+  app.listen(port, () => {
+    console.log(`Server is running http://localhost:${port}`);
+  });
 });
